490d467da53fdd1f1d83b0ac8430129b1408f0eb
Igor Cardoso <iygorsilva@gmail.com>
2022-01-16 17:25:26 -0300

    route app settings

--- netflix-fake-client/src/App.tsx
+++ netflix-fake-client/src/App.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import Login from "./screens/login/login.screen";
+import { Route, Routes } from "react-router-dom";
+import { LoginPath } from "./screens/login/login.types"
+
+
 import './App.css';
 
 function App() {
   const myText = "Olá";
 
   return (
-    <div id={myText} className="test-text">
-      <header>
-        <b>{myText}</b>
-      </header>
-    </div>
+    // // Exemplo
+    // <div id={myText} className="test-text">
+    //   <header>
+    //     <b>{myText}</b>
+    //     {<Login></Login>}
+    //   </header>
+    // </div>
+
+    <Routes>
+      {/* <Route path="/login" element={<Login />} /> */}
+      <Route path={LoginPath} element={<Login />} />
+
+    </Routes>
   );
 }
 
